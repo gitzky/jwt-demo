568d3e12dc86457a6ac2696e3c04f59da00f936c
gitzky <927926261@qq.com>
2022-03-23 14:21:25 +0800

    up

--- server/config/index.js
+++ server/config/index.js
@@ -1,7 +1,7 @@
 
 module.exports = {
     connectUrl: 'mongodb://localhost:27017/jwtdemo',
-    port: 3000,
+    port: 8000,
     baseApi: 'api',
     secret: 'jwt_secret',
     unless_path: [/\/api\/register/, /\/api\/login/]
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ export default {
     port: 8080,//指定客户端默认端口
     proxy: {
         "/api": {
-            target: "http://localhost:3000", //代理服务端域名端口
+            target: "http://localhost:8000", //代理服务端域名端口
             changeOrigin: true,
         },
     },
